b5431187cb16709acaec2d977d05dd7630b8eed7
Oruluk <pat.dein@gmail.com>
2017-10-07 05:01:24 +0200

    Added InsuranceModule

--- client/components/ModulePage/ModulePage.js
+++ client/components/ModulePage/ModulePage.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import Page from 'components/Page/Page';
 import styles from './ModulePage.scss';
+import InsuranceModule from 'components/InsuranceModule/InsuranceModule';
+
+
+const data = [{
+  type: 'Life',
+  premium: 10,
+  description: 'asdf',
+  coverage: ['suicide', 'accidential death', 'murder']
+},
+{
+  type: 'House',
+  premium: 7,
+  description: 'qwert',
+  coverage: ['jewelry', 'fine art', 'bikes']
+}
+]
+
+
 
 class ModulePage extends React.Component{
   render(){
@@ -8,6 +26,8 @@ class ModulePage extends React.Component{
       <Page title='' viewer={this.props.viewer}>
         <section className={styles.container}>
           <h1>Fancy Insurances</h1>
+          <InsuranceModule data={data[0]}/>
+          <InsuranceModule data={data[1]}/>
         </section>
       </Page>
     );
