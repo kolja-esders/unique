c494988c4af3d7f7397097104927a5185f301f15
david <d.zimmerer@gmail.com>
2017-10-07 12:12:33 +0200

    made scraping more api like (less scripty) xD

--- server/your_insurance/helpers/fb/scraper.py
+++ server/your_insurance/helpers/fb/scraper.py
@@ -22,8 +22,11 @@ import urllib
 import pprint
 
 # get Facebook access token from environment variable
-ACCESS_TOKEN = 'EAACEdEose0cBAFGP6cZAugLyZALbZBvnxA7ab5wtjq2NNtZAiWvzNJhUkDqC2eljZCpBewREQq4JZAmOlwmgkSd8Lo2FQpT8rchyU1mzHMeUKnnNJB0ObyDiMYKcrWrqix2Eio5ssNdf83ZAe1b4LUMUVD6pqZAIx3oNIXjVOZBW3eBxzDla95oWDYoNWIgVA1QwWL0eSIZBa6kQZDZD'
+ACCESS_TOKEN = 'EAACEdEose0cBAHnevxRJS5azvxeRGWgUdjSJzZAvdGosQ1kCEA8TAFxoesfvhZBjNZCWiEyBrAEMYtptDJveSPvSRx2QpSFn3qT9UB50j52Axx0FNVlINNfZCfEo2c4IPL6tfO0MJCVUVpO6qn63B0QfjAZCLs9knT9dImjS3FZAWfUmWWCWScUISYp3PjgLJXs0dTAuibmwZDZD'
 HOST = "https://graph.facebook.com"
+
+N_IMGS = 10
+N_POSTS = 10
 
 
 # build the URL for the API endpoint
@@ -74,51 +77,106 @@ def some_action(post):
 # Look at Bill Gates's profile for this example by using his Facebook id.
 user = 'BillGates'
 
-graph = facebook.GraphAPI(ACCESS_TOKEN)
-profile = graph.get_object(user)
-posts = graph.get_connections(profile['id'], 'posts')
+def get_posts_for_uid(uid):
 
-photos = graph.get_connections(profile['id'], 'photos')
+    graph = facebook.GraphAPI(ACCESS_TOKEN)
+    profile = graph.get_object(user)
+    posts = graph.get_connections(profile['id'], 'posts')
+
+    ret_posts = []
+
+    for i in range(N_POSTS):
+        try:
+            # Perform some action on each post in the collection we receive from
+            # Facebook.
+            for post in posts['data']:
+                ret_posts.append(post['message'])
+
+            # Attempt to make a request to the next page of data, if it exists.
+            posts = requests.get(posts['paging']['next']).json()
+        except KeyError:
+            # When there are no more pages (['paging']['next']), break from the
+            # loop and end the script.
+            break
+
+    return ret_posts
 
 
+def get_images_for_uid(uid):
+
+    graph = facebook.GraphAPI(ACCESS_TOKEN)
+    profile = graph.get_object(user)
+    photos = graph.get_connections(profile['id'], 'photos')
+
+    ret_imgs = []
+
+    for i in range(N_IMGS):
+        try:
+            # Perform some action on each post in the collection we receive from
+            # Facebook.
+            for photo in photos['data']:
+                ret_imgs.append(dict(
+                    img_url = get_img_url_for_id(photo['id']),
+                    name = photo.get("name")
+                ))
+
+            # Attempt to make a request to the next page of data, if it exists.
+            photos = requests.get(photos['paging']['next']).json()
+        except KeyError:
+            # When there are no more pages (['paging']['next']), break from the
+            # loop and end the script.
+            break
 
 
-# Wrap this block in a while loop so we can keep paginating requests until
-# finished.
-def some_action2(post):
-
-    photo_id = post['id']
-
-    print(photo_id)
-
-    get_img_url_for_id(photo_id)
-
-    print("\n")
+print(get_posts_for_uid("BillGates"))
+print(get_images_for_uid("BillGates"))
 
 
-
-while True:
-    try:
-        # Perform some action on each post in the collection we receive from
-        # Facebook.
-        [some_action2(post=post) for post in photos['data']]
-        # Attempt to make a request to the next page of data, if it exists.
-        posts = requests.get(posts['paging']['next']).json()
-    except KeyError:
-        # When there are no more pages (['paging']['next']), break from the
-        # loop and end the script.
-        break
-
-# Wrap this block in a while loop so we can keep paginating requests until
-# finished.
-while True:
-    try:
-        # Perform some action on each post in the collection we receive from
-        # Facebook.
-        [some_action(post=post) for post in posts['data']]
-        # Attempt to make a request to the next page of data, if it exists.
-        posts = requests.get(posts['paging']['next']).json()
-    except KeyError:
-        # When there are no more pages (['paging']['next']), break from the
-        # loop and end the script.
-        break
+# graph = facebook.GraphAPI(ACCESS_TOKEN)
+# profile = graph.get_object(user)
+# posts = graph.get_connections(profile['id'], 'posts')
+#
+# photos = graph.get_connections(profile['id'], 'photos')
+#
+#
+#
+#
+# # Wrap this block in a while loop so we can keep paginating requests until
+# # finished.
+# def some_action2(post):
+#
+#     photo_id = post['id']
+#
+#     print(photo_id)
+#
+#     get_img_url_for_id(photo_id)
+#
+#     print("\n")
+#
+#
+#
+# while True:
+#     try:
+#         # Perform some action on each post in the collection we receive from
+#         # Facebook.
+#         [some_action2(post=post) for post in photos['data']]
+#         # Attempt to make a request to the next page of data, if it exists.
+#         posts = requests.get(posts['paging']['next']).json()
+#     except KeyError:
+#         # When there are no more pages (['paging']['next']), break from the
+#         # loop and end the script.
+#         break
+#
+# # Wrap this block in a while loop so we can keep paginating requests until
+# # finished.
+# while True:
+#     try:
+#         # Perform some action on each post in the collection we receive from
+#         # Facebook.
+#         [some_action(post=post) for post in posts['data']]
+#         # Attempt to make a request to the next page of data, if it exists.
+#         posts = requests.get(posts['paging']['next']).json()
+#     except KeyError:
+#         # When there are no more pages (['paging']['next']), break from the
+#         # loop and end the script.
+#         break
