b2761862673d66eecca30001f0758ea679a91ad8
Oruluk <pat.dein@gmail.com>
2017-10-07 15:57:53 +0200

    Small adaptions in ModulePage

--- client/components/InsuranceModule/InsuranceModule.js
+++ client/components/InsuranceModule/InsuranceModule.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './InsuranceModule.scss';
-import { Grid, Button } from 'semantic-ui-react';
+import { Grid, Button, Progress, Rating } from 'semantic-ui-react';
 
 
 class InsuranceModule extends React.Component{
@@ -20,6 +20,27 @@ class InsuranceModule extends React.Component{
             </Grid.Column>
             <Grid.Column>
                 {this.props.data.coverage[2]}
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+
+
+        <Grid divided='vertically'>
+          <Grid.Row columns={3}>
+            <Grid.Column>
+                {this.props.data.similar[0]}
+                 <Rating defaultRating={5} maxRating={5} disabled />
+                 <Progress percent={this.props.data.fit[0]} progress color='blue'/>
+            </Grid.Column>
+            <Grid.Column>
+                {this.props.data.similar[1]}
+                 <Rating defaultRating={4} maxRating={5} disabled />
+                <Progress percent={this.props.data.fit[1]} progress color='blue'/>
+            </Grid.Column>
+            <Grid.Column>
+                {this.props.data.similar[2]}
+                 <Rating defaultRating={3} maxRating={5} disabled />
+                 <Progress percent={this.props.data.fit[2]} progress color='blue'/>
             </Grid.Column>
           </Grid.Row>
         </Grid>
