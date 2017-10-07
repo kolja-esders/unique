95ceaab049c2b2dd3cfe28ac32ea65a15c134041
Thomas <thomas.fitz91@gmail.com>
2017-10-07 05:22:45 +0200

    Loading Person Data - Still some errors

--- server/core/models.py
+++ server/core/models.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.contrib.auth import get_user_model
 from custom_user.models import AbstractEmailUser
 
-class Book(models.Model):
+'''sysyclass Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
 
@@ -43,3 +43,44 @@ class Membership(models.Model):
 
     class Meta:
         unique_together = ('user', 'group')
+'''
+
+class CustomUser(AbstractEmailUser):
+    lfd_id = models.CharField(max_length=12, blank=True)
+    parent_lfd_id = models.CharField(max_length=12, blank=True)
+    pnr_nr = models.CharField(max_length=12, blank=True)
+    title = models.CharField(max_length=12, blank=True)
+    given_name = models.CharField(max_length=128, blank=True)
+    surname = models.CharField(max_length=128, blank=True)
+    gender = models.CharField(max_length=32, blank=True)
+    email_address = models.CharField(max_length=256, blank=True)
+    browser_user_agent = models.CharField(max_length=512, blank=True)
+    telephone_number = models.CharField(max_length=64, blank=True)
+    telephone_country_code = models.CharField(max_length=12, blank=True)
+    birthday = models.CharField(max_length=24, blank=True)
+    age = models.CharField(max_length=3, blank=True)
+    tropical_zodiac = models.CharField(max_length=64, blank=True)
+    cc_type = models.CharField(max_length=64, blank=True)
+    cc_number = models.CharField(max_length=64, blank=True)
+    cvv2 = models.CharField(max_length=12, blank=True)
+    cc_expires = models.CharField(max_length=64, blank=True)
+    occupation = models.CharField(max_length=256, blank=True)
+    company = models.CharField(max_length=256, blank=True)
+    vehicle = models.CharField(max_length=256, blank=True)
+    domain = models.CharField(max_length=256, blank=True)
+    blood_type = models.CharField(max_length=12, blank=True)
+    pounds = models.CharField(max_length=12, blank=True)
+    kilograms = models.CharField(max_length=12, blank=True)
+    feet_inches = models.CharField(max_length=12, blank=True)
+    centimeters = models.CharField(max_length=3, blank=True)
+    profile_picture = models.CharField(max_length=512, blank=True)
+    maried = models.CharField(max_length=64, blank=True)
+    kids = models.CharField(max_length=12, blank=True)
+    income = models.CharField(max_length=64, blank=True)
+    expenses = models.CharField(max_length=64, blank=True)
+    fitness = models.CharField(max_length=128, blank=True)
+    activities = models.CharField(max_length=512, blank=True)
+    home_town = models.CharField(max_length=512, blank=True)
+    country = models.CharField(max_length=256, blank=True)
+    education = models.CharField(max_length=256, blank=True)
+    devices = models.CharField(max_length=512, blank=True)
