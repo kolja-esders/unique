453a7983c25961e8c07ed8b3c9b140c6c0b9b064
Oruluk <pat.dein@gmail.com>
2017-10-07 03:11:00 +0200

    Added further details to LoadingPage

--- client/components/LoadingPage/LoadingPage.js
+++ client/components/LoadingPage/LoadingPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Loader } from 'semantic-ui-react';
+import { Loader, Progress, Segment, Button } from 'semantic-ui-react';
 import Page from 'components/Page/Page';
 import styles from './LoadingPage.scss'
 
@@ -9,9 +9,21 @@ class LoadingPage extends React.Component{
       <Page title="Becoming Friends ;-)" viewer={this.props.viewer}>
         <section className={styles.container}>
 
-            <h1>Lets become Friends!</h1>
-            <div className={styles.emoji}></div>
+            <div className={styles.root}>
+
+                <Segment padded='very' className={styles.becomingFriends}>
+                  <div className={styles.emoji}></div>
+                  <div className={styles.friends}>
+                    <h1>Let's become Friends!</h1>
+                    <Progress percent={60} active>
+                      Scanning social media profiles...
+                    </Progress>
+                  </div>
+                </Segment>
+            </div>
+
         </section>
+
       </Page>
 
     );
