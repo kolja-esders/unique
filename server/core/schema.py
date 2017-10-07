b4e9d8abfa044b7aa3df941b295eee5176725ca4
Thomas <thomas.fitz91@gmail.com>
2017-10-07 18:29:07 +0200

    Some of the Queries

--- server/core/schema.py
+++ server/core/schema.py
@@ -5,6 +5,7 @@ from graphene_django.types import DjangoObjectType, ObjectType
 from core.user_helper.jwt_util import get_token_user_id
 from core.user_helper.jwt_schema import TokensInterface
 from .models import Book as BookModal, BookshelfEntry as BookshelfEntryModal, Membership as MembershipModal, Group as GroupModal
+from .models import DetectionReason as DetectionReasonModal, Device as DeviceModal, Activity as ActivityModal, FamilyMember as FamilyMemberModal, LifestyleEntity as LifestyleEntityModal, Location as LocationModal, Injury as InjuryModal, Person as PersonModal, Contract as ContractModal, Review as ReviewModal
 
 class Book(DjangoObjectType):
     class Meta:
@@ -53,7 +54,91 @@ class User(DjangoObjectType):
     def resolve_bookshelf(self):
         return self.bookshelfentry_set.all()
 
+class DetectionReason(DjangoObjectType):
+    class Meta:
+        model = DetectionReasonModal
+        interfaces = (graphene.Node, )
+
+class Device(DjangoObjectType):
+    class Meta:
+        model = DeviceModal
+        interfaces = (graphene.Node, )
+
+class Activity(DjangoObjectType):
+    class Meta:
+        model = ActivityModal
+        interfaces = (graphene.Node, )
+
+class FamilyMember(DjangoObjectType):
+    class Meta:
+        model = FamilyMemberModal
+        interfaces = (graphene.Node, )
+
+class LifestyleEntity(DjangoObjectType):
+    class Meta:
+        model = LifestyleEntityModal
+        interfaces = (graphene.Node, )
+
+class Location(DjangoObjectType):
+    class Meta:
+        model = LocationModal
+        interfaces = (graphene.Node, )
+
+
 class CoreQueries(graphene.AbstractType):
+    detection_reason = graphene.Node.Field(DetectionReason)
+    detection_reasons = graphene.List(DetectionReason)
+    all_detection_reasons = DjangoFilterConnectionField(DetectionReason)
+
+    device = graphene.Node.Field(Device)
+    devices = graphene.List(Device)
+    all_devices = DjangoFilterConnectionField(Device)
+
+    activity = graphene.Node.Field(Activity)
+    activities = graphene.List(Activity)
+    all_activities = DjangoFilterConnectionField(Activity)
+
+    family_member = graphene.Node.Field(FamilyMember)
+    family_members = graphene.List(FamilyMember)
+    all_family_members = DjangoFilterConnectionField(FamilyMember)
+
+    lifestyle_entity = graphene.Node.Field(LifestyleEntity)
+    lifestyle_entities = graphene.List(LifestyleEntity)
+    all_lifestyle_entities = DjangoFilterConnectionField(LifestyleEntity)
+
+    location = graphene.Node.Field(Location)
+    locations = graphene.List(Location)
+    all_locations = DjangoFilterConnectionField(Location)
+
+
+
+    def resolve_detection_reasons(self, args, context, info):
+        detection_reasons = DetectionReasonModal.objects.all()
+        return detection_reasons
+
+    def resolve_devices(self, args, context, info):
+        devices = DeviceModal.objects.all()
+        return devices
+
+    def resolve_activities(self, args, context, info):
+        activities = ActivityModal.objects.all()
+        return activities
+
+    def resolve_family_members(self, args, context, info):
+        family_members = FamilyMemberModal.objects.all()
+        return family_members
+
+    def resolve_lifestyle_entities(self, args, context, info):
+        lifestyle_entities = LifestyleEntityModal.objects.all()
+        return lifestyle_entities
+
+    def resolve_locations(self, args, context, info):
+        locations = LocationModal.objects.all()
+        return locations
+
+
+
+
     book = graphene.Node.Field(Book)
     books = graphene.List(Book)
     all_books = DjangoFilterConnectionField(Book)
