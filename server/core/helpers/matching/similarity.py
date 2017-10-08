ba5137ebe796991f0dea82b96b89efe3c8472fbb
david <d.zimmerer@gmail.com>
2017-10-08 04:55:26 +0200

    Added similar stuff to a person

--- server/core/helpers/matching/similarity.py
+++ server/core/helpers/matching/similarity.py
@@ -1,9 +1,11 @@
 from collections import Counter
 
+from core.models import Activity, LifestyleEntity
+
 
 def get_jac_similarity(model1, model2):
-    model1_dict = model1.__dict__()
-    model2_dict = model2.__dict__()
+    model1_dict = model1.__dict__
+    model2_dict = model2.__dict__
 
     similarity_cntr = 0
 
@@ -34,5 +36,26 @@ def get_dict_similarity(dict1, dict2):
 
 def get_list_similarity(list1, list2):
 
-    c = list((Counter(list) & Counter(list2)).elements())
+    c = list((Counter(list1) & Counter(list2)).elements())
     return len(c)
+
+
+def get_person_similarity(p0, p1):
+
+    sim = get_dict_similarity(p0.__dict__, p1.__dict__)
+
+    a0 = list(Activity.objects.filter(person_id=p0.id))
+    a1 = list(Activity.objects.filter(person_id=p1.id))
+
+    l0 = list(LifestyleEntity.objects.filter(person_id=p0.id))
+    l1 = list(LifestyleEntity.objects.filter(person_id=p1.id))
+
+    a0 = set([a.name for a in a0])
+    a1 = set([a.name for a in a1])
+    l0 = set([l.name for l in l0])
+    l1 = set([l.name for l in l1])
+
+    sim += get_list_similarity(l0, l1)
+    sim += get_list_similarity(a0, a1)
+
+    return sim
