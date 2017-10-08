7d9163ba0ff87bc5ea9c9ce3a49ff24a93edf3a8
Oruluk <pat.dein@gmail.com>
2017-10-08 06:52:57 +0200

    Design improvements for SimilarityPopup

--- client/components/SimilarityPopup/SimilarityPopup.js
+++ client/components/SimilarityPopup/SimilarityPopup.js
@@ -7,16 +7,16 @@ class SimilarityPopup extends React.Component{
   render(){
     return(
       <div>
-        <Grid centered divided columns={1}>
+        <Grid centered divided columns={1} className={styles.grid}>
           <Grid.Column textAlign='center'>
             <Header as='h4'>{this.props.data.similar[0]}</Header>
-            <Image className={styles.picture} src={this.props.data.pictures[0]} size='tiny' shape='circular' />
+
 
             <p>Software Enigineer</p>
             <p>32</p>
             <p>Married</p>
             <p>Rating: <Rating defaultRating={3} maxRating={5} disabled /></p>
-            <p>Similarity: <Progress className={styles.progress} percent={this.props.data.fit[0]} progress color='blue'/></p>
+            <p>Similarity: <Progress className={styles.progress} percent={this.props.data.fit[0]} progress color='blue' size="small"/></p>
 
           </Grid.Column>
         </Grid>
