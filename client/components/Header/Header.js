5e7d0cb1674fae2fa33835458d1cbfe592b521f7
Kolja Esders <kolja.esders@gmail.com>
2017-10-08 03:15:44 +0200

    Updated header

--- client/components/Header/Header.js
+++ client/components/Header/Header.js
@@ -16,22 +16,9 @@ class Header extends React.Component {
     return (
       <header className={styles.root}>
           <h1 className={styles.brand_name}>
-              <Link to="/" className={styles.brand_name_link}>Your Insurance</Link>
+            <Button primary as={Link} to='/home' className={styles.item}>Recommendations</Button>
+            <Button primary as={Link} to='/feed' className={styles.item}>Feed</Button>
           </h1>
-          <nav className={styles.nav}>
-              { isLoggedIn ? (
-                  <div>
-                  <Button primary as={Link} to='/home' className={styles.item}>Recommendations</Button>
-                  <Button primary as={Link} to='/feed' className={styles.item}>Feed</Button>
-                  <Button basic primary className={styles.item} onClick={() => {logoutViewer()}} >Log out</Button>
-                </div>
-              ) : (
-                <div>
-                  <Button primary as={Link} to='/home' className={styles.item}>Insurances</Button>
-                  <Button basic as={Link} to='/feed' className={styles.item}>Feed</Button>
-                </div>
-              )}
-          </nav>
       </header>
     )
   }
